d6a6d95b2deddc3090d156feba2d6a0cc2ee3b49
BaoNguyen777 <129492539+BaoNguyen777@users.noreply.github.com>
2026-09-12 08:53:06 +0700

    chore: configure remote resort images

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "cf.bstatic.com" }]
+  }
+};
+
+export default nextConfig;
